cf7d05bb7ae20a9bedc82169806ecf9c32edad34
Tianxiao Zhao <Tianxiao.Zhao@nyulangone.org>
2025-09-10 10:56:34 -0400

    Release version 0.2.5: bug fixes on spatial fitting

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="simspace",
-    version="0.2.4",
+    version="0.2.5",
     description="SimSpace: a comprehensive in-silico spatial omics data simulation framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
